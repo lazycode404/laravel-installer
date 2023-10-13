075aecf1331e2af2463013554d0467e530b22ddb
LazyCode404 <troyventura00@gmail.com>
2023-10-14 02:49:14 +0800

    Initial Commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lazycode404/laravelwebinstaller',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '15ee6313572a8855329779dc603d1123a17184d0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lazycode404/laravelwebinstaller' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '15ee6313572a8855329779dc603d1123a17184d0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
